e14a09b2b5a6c4b7d0bc6f12f853e2ef40660cde
bruno <brunoandreribeiro18@gmail.com>
2024-08-31 13:03:10 +0100

    added api layer

--- logging.go
+++ logging.go
@@ -18,7 +18,7 @@ func NewLoggingService(next Service) Service {
 
 func (s *LoggingService) GetCatFact(ctx context.Context) (fact *CatFact, err error) {
 	defer func(start time.Time) {
-		fmt.Printf("fact=%v err=%v took=%v", fact, err, time.Since(start))
+		fmt.Printf("fact=%v err=%v took=%v\n", fact.Fact, err, time.Since(start))
 	}(time.Now())
 
 	return s.next.GetCatFact(ctx)
